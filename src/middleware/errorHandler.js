3ed58baf068ae1d8910e25c7dd2673f63ca531fa
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-02-06 17:17:44 -0300

    feat: error Handler functions

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,39 @@
+import AppError from '../errors/AppError/AppError.js';
+import BadRequest from '../errors/BadRequest/BadRequest.js';
+import JwtInvalidError from '../errors/JwtInvalidError/JwtInvalidError.js';
+import JwtExpiredError from '../errors/JwtExpiredError/JwtExpiredError.js';
+import InternalServerError from '../errors/InternalServerError/InternalServerError.js';
+import ConflictError from '../errors/ConflictError/ConflictError.js';
+import logger from '../config/logger.js';
+import { z } from 'zod';
 
 
+const userLogin = z.object ({
+    email: z.string().email(),
+    password: z.string().passmax(16).min(6),
+})
+
+const formsContact = z.object ({
+    company: z.string(),
+    representative: z.string(),
+    email: z.string().email(),
+    telephone: z.string().max(15),
+    menssage: z.string().max(1500).min(20),
+});
+
+const formsBudget = z.object ({
+    name: z.string().max(40).min(10),
+    company: z.string(),
+    email: z.string().email(),
+    telephone: z.string().max(15),
+    country: z.string().max(30).min(4),
+    state: z.string().max(30).min(4),
+    city: z.string().max(30).min(4),
+    ZIPcode: z.string().max(8).min(5),
+    address: z.string().max(50).min(5),
+});
 
 const errorHandler = (err, req, res, next) => {
+    let error;
 };
 export default errorHandler;
