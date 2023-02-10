4938119b438c999f49a9fda151846b1eebfc6a64
Alexandre Cenachi <alexandrecenachi@cpejr.com.br>
2023-02-10 08:57:32 -0300

    fix: adding post and delete index on the user validator

---
+++ src/validators/UserValidator.js
@@ -0,0 +1,90 @@
+import { z } from 'zod';
+import validate from '../config/validate.js';
+
+export const getUserValidator = validate(z.object());
+export const createUserValidator = validate(
+  z.object({
+    name: z
+      .string({ required_error: 'Name is required' })
+      .max(40, { message: 'Name must be a maximum of 40 characters' })
+      .min(10, { message: 'Name must be atleast 10 characters' }),
+
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email('Must be a valid email'),
+
+    password: z
+      .string({ required_error: 'Password is required' })
+      .max(16, {
+        message: 'Password must be a maximum of 16 characters',
+      })
+      .min(6, { message: 'Password must be atleast 6 characters' }),
+  })
+);
+export const updateUserValidator = validate(z.object());
+export const deleteUserValidator = validate(
+  z.object({
+    name: z
+      .string({ required_error: 'Name is required' })
+      .max(40, { message: 'Name must be a maximum of 40 characters' })
+      .min(10, { message: 'Name must be atleast 10 characters' }),
+
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email('Must be a valid email'),
+
+    password: z
+      .string({ required_error: 'Password is required' })
+      .max(16, {
+        message: 'Password must be a maximum of 16 characters',
+      })
+      .min(6, { message: 'Password must be atleast 6 characters' }),
+  })
+);
+// export const formsBudgetValidator = validate(
+//   z.object({
+//     body: z.object({
+//       name: z
+//         .string({ required_error: 'Name is required' })
+//         .max(40, { message: 'Name must be a maximum of 40 characters' })
+//         .min(10, { message: 'Name must be atleast 10 characters' }),
+
+//       company: z.string({ required_error: 'Company name is required' }),
+
+//       email: z
+//         .string({ required_error: 'Email is required' })
+//         .email('Must be a valid email'),
+
+//       telephone: z
+//         .string({ required_error: 'Telephone is required' })
+//         .max(15, { message: 'Telephone must be a maximum of 15 characters' })
+//         .regex(/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Zip code bad formatted'),
+
+//       country: z
+//         .string({ required_error: 'Country is required' })
+//         .max(30, { message: 'Country must be a maximum of 30 characters' })
+//         .min(4, { message: 'Country must be atleast 4 characters' }),
+
+//       state: z
+//         .string({ required_error: 'State is required' })
+//         .max(30, { message: 'State must be a maximum of 30 characters' })
+//         .min(4, { message: 'State must be atleast 4 characters' }),
+
+//       city: z
+//         .string({ required_error: 'City is required' })
+//         .max(30, { message: 'City must be a maximum of 30 characters' })
+//         .min(4, { message: 'City must be atleast 4 characters' }),
+
+//       ZIPcode: z
+//         .string({ required_error: 'ZIPcode is required' })
+//         .max(8, { message: 'ZIPcode must be a maximum of 8 characters' })
+//         .min(5, { message: 'ZIPcode must be atleast 5 characters' })
+//         .regex(/^[0-9]{5}(?:-[0-9]{4})?$/, 'Zip code bad formatted'),
+
+//       address: z
+//         .string({ required_error: 'Adress is required' })
+//         .max(50, { message: 'Adress must be a maximum of 50 characters' })
+//         .min(5, { message: 'Adress must be atleast 5 characters' }),
+//     }),
+//   })
+// );
