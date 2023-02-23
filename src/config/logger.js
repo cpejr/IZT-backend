ccd4829b95bc41acf94d0d44592519e71dda39fe
joaopedropiraja <jplp100@hotmail.com>
2023-02-22 23:50:30 -0300

    fix: logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,21 +1,14 @@
 import pino from 'pino';
-import isDevEnvironment from '../utils/isDevEnvironment.js';
 
-function getLogger() {
-  if (isDevEnvironment)
-    return pino({
-      base: { pid: false },
-      level: process.env.PINO_LOG_LEVEL || 'info',
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'",
-        },
-      },
-    });
+const logger = pino({
+  base: { pid: false },
+  level: process.env.PINO_LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'",
+    },
+  },
+});
 
-  return console;
-}
-
-const logger = getLogger();
 export default logger;
