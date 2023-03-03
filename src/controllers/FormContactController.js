02a06e061ed69048a8650116da6bb510a1409423
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-03-02 21:52:15 -0300

    feat: styled email

--- src/controllers/FormContactController.js
+++ src/controllers/FormContactController.js
@@ -9,7 +9,37 @@ const sendFormContactEmail = asyncHandler(async (req, res) => {
     to: 'alexandrecenachi@cpejr.com.br',
     subject: '[IZT] - Formulário de contato',
     text: 'Here is the text of the test email',
-    html: `<h1>${Object.entries(inputData).toString()}</h1>`,
+    html: `
+    <style>
+    h1 {
+      font-size: 24px;
+      font-weight: bold;
+    }
+    </style>
+
+    <h1>These are the informations about a new client</h1>
+    <table>
+      <tr>
+        <td>Company: </td>
+        <td>${inputData.company}</td>
+      </tr>
+
+      <tr>
+        <td>Email: </td>
+        <td>${inputData.email}</td>
+      </tr>
+
+      <tr>
+        <td>Telephone: </td>
+        <td>${inputData.telephone}</td>
+      </tr>
+
+      <tr>
+        <td>Message: </td>
+        <td>${inputData.message}</td>
+      </tr>
+    </table>
+  `,
   };
   await Email.sendEmail(mailOptions);
   res.sendStatus(SUCCESS_CODES.OK);
